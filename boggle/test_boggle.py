8f5765669cd53d5e9626cc9d9f44c01bea881ac3
saffiya <saffiyax@icloud.com>
2019-10-23 10:13:26 +0000

    initial commit

--- boggle/test_boggle.py
+++ boggle/test_boggle.py
@@ -1,5 +1,6 @@
 import unittest
 import boggle
+from string import ascii_uppercase
 
 
 class TestBoggle(unittest.TestCase):
@@ -29,3 +30,12 @@ class TestBoggle(unittest.TestCase):
         self.assertIn((1,0), grid)
         self.assertIn((1,1), grid)
         self.assertNotIn((2,2), grid)
+
+    def test_grid_is_filled_with_letters(self):
+        """
+        Ensure that each of the coordinators in the grid contains letters
+        """
+        grid = boggle.make_grid(2,3)
+        for letter in grid.values():
+            self.assertIn(letter, ascii_uppercase)
+
