1456c6489b9877d552973f4e80437e68d49e379b
saffiya <saffiyax@icloud.com>
2019-10-26 14:35:05 +0000

    changed grid size. changed lists into stems and modified the search function to use full words to check whether we find a real word

--- boggle/boggle.py
+++ boggle/boggle.py
@@ -48,16 +48,20 @@ def path_to_word(grid, path):
     Add all of the letters on the path to a string
     """
     return ''.join([grid[p] for p in path])
+
 def search(grid, dictionary):
     """
     Search through the paths to locate words by matching strings to words in a dictionary
     """
     neighbours = all_grid_neighbours(grid)
     paths = []
+    full_words, stems = dictionary
     def do_search(path):
         word = path_to_word(grid, path)
-        if word in dictionary:
+        if word in full_words:
             paths.append(path)
+        if word not in stems:
+            return
         for next_pos not in neighbours[path[-1]]:
             if next_pos not in path:
                 do_search(path + [next_pos])
@@ -71,13 +75,21 @@ def get_dictionary(dictionary_file):
     """
     Load Dictionary file
     """
+    full_words, stems = set(), set()
+
     with open(dictionary_file) as f:
-        return [w.strip().upper() for w in f]
+        for word in f:
+            word = word.strip().upper()
+            full_words.add(word)
+            for i in range(1, len(word)):
+                stems.add(word[:i])
+
+    return full_words, stems
 def main():
     """
     This is the function that will run the ehwole project
     """
-    grid = make_grid(3,3)
+    grid = make_grid(4,4)
     dictionary = get_dictionary('words.txt')
     words = search(grid, dictionary)
     for words in words:
--- boggle/test_boggle.py
+++ boggle/test_boggle.py
@@ -82,7 +82,9 @@ class TestBoggle(unittest.TestCase):
     twoLetterWord = 'AB'
     threeLetterWord = 'ABC'
     notThereWord = 'EEE'
-    dictionary = [twoLetterWord, threeLetterWord, notThereWord]
+    fullwords = [twoLetterWord, threeLetterWord, notThereWord]
+    stems = ['A', 'AB', 'E', 'EE']
+    dictionary = fullwords, stems
     foundWords = boggle.search(grid, dictionary)
     self.assertTrue(twoLetterWord in foundWords)
     self.assertTrue(threeLetterWord in foundWords)
